5b668abecca1a0de23993b7753f1a609b11883d0
QuangHung <duongquanghung0122@gmail.com>
2022-08-27 23:20:04 +0700

    Done

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://qh-learnit.herokuapp.com/api"
+    ? "http://localhost:5000/api"
     : "somedeployURL";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
